9159298cd3f2d759fddc6d2f4be634d99b4366ce
3111234 <3111234@TE-GDA-D-D12C03>
2022-03-25 13:18:39 +0100

    Dodaj pliki projektów.

---
+++ Szyfr Cezara/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Wisz wiadomość do zakodowania");
+string wiadomosc = Console.ReadLine();
+char[] wchar = wiadomosc.ToCharArray();
+int a;
+Console.WriteLine("wpisz klucz");
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 0; i < wchar.Length; i++)
+{
+     wchar[i] = (char)((char)wchar[i] + a);
+        if (wchar[i] > 90)
+        {
+            wchar[i] = (char)((char)wchar[i] - 26);
+        }
+}
+string wiadomosc2 = new string(wchar);
+Console.WriteLine("zaszyfrowana wiadomosc");
+Console.WriteLine(wiadomosc2);
+
